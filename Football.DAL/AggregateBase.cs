220319b5fdc145b1479ca8e6ef5f1502efe5720e
Oleksiy Shovkoplyas <Oleksiy_Shovkoplyas@epam.com>
2018-12-10 10:57:36 +0200

    Add project files.

---
+++ Football.DAL/AggregateBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football.DAL
+{
+    public abstract class AggregateBase : IAggregate
+    {
+        private readonly List<object> _pendingEvents = new List<object>();
+
+        public abstract object Identifier { get; }
+        public int Version { get; private set; } = -1;
+
+        void IAggregate.ApplyEvent(object @event)
+        {
+            ((dynamic)this).Apply((dynamic)@event);
+            Version++;
+        }
+
+        public ICollection<object> GetPendingEvents()
+        {
+            return _pendingEvents;
+        }
+
+        public void ClearPendingEvents()
+        {
+            _pendingEvents.Clear();
+        }
+
+        protected void RaiseEvent(object @event)
+        {
+            ((IAggregate)this).ApplyEvent(@event);
+            _pendingEvents.Add(@event);
+        }
+    }
+}
